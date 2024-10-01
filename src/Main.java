2f05e4e53d83f7dd3df9aa8c9e36cf740a71f568
Juansehs <you@example.com>
2024-10-01 12:59:12 -0500

    cambios de elementos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//Juan Sebastian Hernandez Silva//
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        HashMap<String, Integer> edades = new HashMap<String, Integer>();
+
+//Añadir o crear elementos asocidados "nombre y edad"//
+        edades.put("Aylen", 15);
+        edades.put("Maye", 20);
+        edades.put("Marissa", 19);
+        edades.put("Terry", 18);
+        edades.put("Jonathan", 19);
+        edades.put("Valentina",20);
+        edades.put("Jess",19);
+        edades.put("Juram",35);
+        edades.put("Juanse",17);
+
+
+// Mostrar en consola el Hash map//
+        System.out.println("Hashmap" + edades);
+
+        //Obtener la edad de la clave ""Aylen"//
+        int edadAylen = edades.get("Aylen");
+        System.out.println("La edad de Aylen es:" + edadAylen);
+
+//Comprobar si dentro del Hashmap esta la clave "Maye"//
+        if (edades.containsKey("Maye")) {
+            System.out.println("Este Hashmap contiene la clave Maye");
+        } else {
+            System.out.println("No contiene la clave escrita");
         }
+        //recorrer los elementos del Hashmap e imprimir clave-valor//
+        for (String nombre : edades.keySet()) {
+            System.out.println(nombre + " tiene " + edades.get(nombre) + " años ");
+        }
+        edades.remove("Terry");
+        System.out.println("Despues de eliminar a Terry quedan en lista:" + edades.size());
+
+// Remplazar una edad y mostrar el hashmap con los cambios//
+        edades.replace("Marissa", 20);
+        edades.replace("Aylen",19);
+        edades.replace("Juanse",20);
+        System.out.println("Despues de los cambios las edades son: "+edades);
+
     }
+
 }
